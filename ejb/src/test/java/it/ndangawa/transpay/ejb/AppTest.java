26fd672e6f78acbddbe0e3eb5968d77130dc2708
ndgwandji <ndanga.wandji@outlook.fr>
2022-05-15 20:30:28 +0200

    setup ejb project module

---
+++ ejb/src/test/java/it/ndangawa/transpay/ejb/AppTest.java
@@ -0,0 +1,20 @@
+package com.digits.transtem.transpay.ejb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
